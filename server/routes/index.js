4627e1d6da34303af26048471828da992a31e405
fmurtadho <fmurtadho@gmail.com>
2018-10-15 14:30:51 +0700

    test tamatamvan 1

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,23 @@
 const router = require('express').Router()
+const router = express.Router()
+const images = require('../helpers/images')
 
 router.get('/',(req,res)=>{
     res.status(201).json({
         message : `
-        /items to get all item
-        
+        Hello
         `
     })
 })
 
+router.post('/upload',
+    images.multer.single('image'), 
+    images.sendUploadToGCS,
+    (req, res) => {
+        res.send({
+        status: 200,
+        message: 'Your file is successfully uploaded',
+        link: req.file.cloudStoragePublicUrl
+    })
+})
 module.exports = router;
